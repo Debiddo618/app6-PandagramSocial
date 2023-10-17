b8dfa41b270f8bbff36e1f893448b78bf7afe53a
David Zheng <67178151+Debiddo618@users.noreply.github.com>
2023-10-16 20:43:23 -0400

    implemented the change and rest password functionality

--- socialproject/users/forms.py
+++ socialproject/users/forms.py
@@ -1,6 +1,11 @@
 from django import forms
-
+from django.contrib.auth.models import User
 
 class LoginForm(forms.Form):
     username = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput)
+
+class UserRegisterForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields={"username","email","first_name"}
