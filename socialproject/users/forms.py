3e87359bae9b3a551f3c2aed67e8afbb32aba3a3
David Zheng <67178151+Debiddo618@users.noreply.github.com>
2023-10-16 20:51:14 -0400

    Created User registration form

--- socialproject/users/forms.py
+++ socialproject/users/forms.py
@@ -6,6 +6,13 @@ class LoginForm(forms.Form):
     password = forms.CharField(widget=forms.PasswordInput)
 
 class UserRegisterForm(forms.ModelForm):
+    password = forms.CharField(label="Password",widget=forms.PasswordInput)
+    password2 = forms.CharField(label="Confirm Password", widget=forms.PasswordInput)
     class Meta:
         model = User
         fields={"username","email","first_name"}
+
+    def check_password(self):
+        if self.cleaned_data["password"]!= self.cleaned_data["password2"]:
+            raise forms.ValidationError("Passwords do not match")
+        return self.cleaned_data["password2"]
