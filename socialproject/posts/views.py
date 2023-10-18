c0d8dd0ade1dc48d31656bec97c2fc59c27d9158
David Zheng <67178151+Debiddo618@users.noreply.github.com>
2023-10-17 22:39:56 -0400

    Implement user post functionality

---
+++ socialproject/posts/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+from .forms import PostCreateForm
+from django.contrib.auth.decorators import login_required
+
+
+@login_required()
+# Create your views here.
+def post_create(request):
+    if request.method == "POST":
+        form = PostCreateForm(data=request.POST, files=request.FILES)
+        if form.is_valid():
+            new_item = form.save(commit=False)
+            new_item.user = request.user
+            new_item.save()
+    else:
+        print(request.method)
+        print("This is the get")
+        print(request.GET)
+        print(request.POST)
+        print(request.META)
+        form = PostCreateForm(data=request.GET)
+    return render(request, "posts/create.html", {"form": form})
+
